6a99482fbc1c34c7ca8516e4b0fe86a3c64121f7
Andrew Gamal <andrewgamal7@gmail.com>
2023-12-06 18:06:35 +0200

    Added IMR and IRR with TB

--- HDL/In_Mask_Reg.v
+++ HDL/In_Mask_Reg.v
@@ -4,6 +4,24 @@
  * Description: 
  ************************************************************/
 
-module In_Mask_Reg();
+module In_Mask_Reg(  
+  input wire write_ICW_1,
+  input wire write_OCW_1,
+  input wire [7:0] Internal_bus_data,
+  output reg [7:0] Interrupt_Mask
+);
+
+always @(*)begin
+  if(write_ICW_1 == 1'b1)begin
+    Interrupt_Mask <= 8'b11111111;
+  end
+  else if(write_OCW_1 == 1'b1) begin
+    Interrupt_Mask <= Internal_bus_data; 
+  end
+  else begin
+    Interrupt_Mask <= Interrupt_Mask;
+  end
+end
+
 
 endmodule
